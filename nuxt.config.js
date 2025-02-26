4496b0f2cf43e7d0c2a469acea11cd93720da44d
aleksandar-stotka <aangelevski6@gmail.com>
2025-02-26 01:30:55 +0100

    again-fix-url

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      graphqlEndpoint: process.env.NUXT_PUBLIC_GRAPHQL_URL || 'https://your-netlify-site.netlify.app/graphql',
+      graphqlEndpoint: process.env.NUXT_PUBLIC_GRAPHQL_URL || 'https://mygrillhouse.netlify.app/graphql',
             API_URL: process.env.NUXT_PUBLIC_API_URL || 'https://mygrillhouse.netlify.app/',
 
     },
