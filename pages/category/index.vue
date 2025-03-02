8a3bf6529cbfca681a9a1028c1abc820e5be3a3d
aleksandar-stotka <aangelevski6@gmail.com>
2025-03-02 22:22:15 +0100

    set-category

---
+++ pages/category/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>category</div>
+</template>
+
+<script setup>
+import { gql } from 'graphql-tag';
+
+const GET_CATEGORY = gql`
+query getCategory($id: ID!) {
+    categories(id:$ id) {
+       name
+       documentId
+       grillmenus {
+      grillmenuimage {
+        url
+      }
+      productname
+      grildescription
+      price
+    }
+    }
+  }
+
+
+`;
+
+
+
+</script>
+
+<style>
+
+</style>
