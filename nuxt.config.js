cfc6422400cbaf88094caef4df66b1e83f5caa80
aleksandar-stotka <aangelevski6@gmail.com>
2025-03-31 17:19:34 +0200

    fix-nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,57 +1,63 @@
-export default defineNuxtConfig({
-  runtimeConfig: {
-    public: {
+  export default defineNuxtConfig({
+    runtimeConfig: {
       public: {
-        SITE_URL: process.env.NUXT_PUBLIC_SITE_URL || 'https://skara-house.com'
+        public: {
+          SITE_URL: process.env.NUXT_PUBLIC_SITE_URL || 'https://skara-house.com'
+        }
+      },
+      gmailUser: process.env.NUXT_GMAIL_USER,
+      gmailPass: process.env.NUXT_GMAIL_PASS
+    },
+    
+      vite: {
+        server: {
+          allowedHosts: ['www.skara-house.com']
+        }
+      },
+      
+    plugins: [
+      '~/plugins/apollo.client.js',
+    ],
+    target: 'static',
+    generate: {
+      fallback: '404.html' // Fixes 404 on reload
+
+    },
+    modules: ['@nuxtjs/tailwindcss'],
+    css: [
+      '@/assets/css/main.css',
+    ],
+    output: 'static',
+    experimental: {
+      payloadExtraction: false
+    },
+    nitro: {
+      prerender: {
+        routes: ['/'] // Make sure the home page is included
       }
     },
-    gmailUser: process.env.NUXT_GMAIL_USER,
-    gmailPass: process.env.NUXT_GMAIL_PASS
-  },
-   
-  plugins: [
-    '~/plugins/apollo.client.js',
-  ],
-  target: 'static',
-  generate: {
-    fallback: '404.html' // Fixes 404 on reload
-
-  },
-  modules: ['@nuxtjs/tailwindcss'],
-  css: [
-    '@/assets/css/main.css',
-  ],
-  output: 'static',
-  experimental: {
-    payloadExtraction: false
-  },
-  nitro: {
-    prerender: {
-      routes: ['/'] // Make sure the home page is included
-    }
-  },
-  routeRules: {
-    '/**': { ssr: false } // Force Nuxt to behave like an SPA
-  },
-  pageTransition: {
-    name: 'page', // Name of the transition
-    mode: 'out-in', // This ensures that the outgoing page fades out before the new page fades in
-  },
-  compatibilityDate: '2025-02-14',
-  theme: {
-    extend: {
-      fontFamily: {
-        barlow: ["Barlow", "sans-serif"],
-      },    
+    routeRules: {
+      '/**': { ssr: false } // Force Nuxt to behave like an SPA
     },
-  },
-  head: {
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/assets/logo' }
-    ]
-  },
-  runtimeConfig: {
-    gmailUser: process.env.NUXT_GMAIL_USER,
-    gmailPass: process.env.NUXT_GMAIL_PASS
-  }
-});
+    pageTransition: {
+      name: 'page', // Name of the transition
+      mode: 'out-in', // This ensures that the outgoing page fades out before the new page fades in
+    },
+    compatibilityDate: '2025-02-14',
+    theme: {
+      extend: {
+        fontFamily: {
+          barlow: ["Barlow", "sans-serif"],
+        },    
+      },
+    },
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/assets/logo.jpg' }
+      ]
+    },
+    runtimeConfig: {
+      gmailUser: process.env.NUXT_GMAIL_USER,
+      gmailPass: process.env.NUXT_GMAIL_PASS
+    }
+  });
