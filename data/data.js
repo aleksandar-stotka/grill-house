f1e7dd2f1f76ea6401a80e86dfaab9ca33c7f4c7
pemierDrum <aleksandar6@gmail.com>
2025-04-03 03:31:06 -0700

    set-menu

---
+++ data/data.js
@@ -0,0 +1,79 @@
+export const menu = [
+{
+    image: "/public/menu/mix-grill/pattern.jpg",
+    title: "Mix Grill Platter",
+    price:19,
+    desk: 'Comes with sausage, chicken, cevapi, pork neck, salad and bread.'
+
+
+
+},
+{
+    image: "/public/menu/cevapi.jpg",
+    title: "Cevapi Burger",
+    price:12,
+    desk: 'Made with beef cevapi, cabbage, ajvar, and sour cream.'
+
+
+},
+{
+    image: "/public/menu/pleskavica.jpg",
+    title: "Pleskavica Burger",
+    price:12,
+    desk: 'Made with beef patty, lettuce, tomato, ajvar and sour cream.'
+
+
+},
+{
+    image: "/public/menu/lovecka.jpg",
+    title:"Lovecka Burger",
+    price: 20,
+    desk: 'Made with beef patty, lettuce, tomato, ajvar and sour cream.'
+
+
+},
+{
+    image: "/public/menu/chicken.jpg",
+    title:"Skara Chicken Burger",
+    price:12,
+    desk: 'Made with grilled chicken, lettuce, cabbage, tomato and mayonnaise.'
+
+
+},
+{
+    image: "/public/menu/wings.jpg",
+    price: 18,
+    title: "Chicken Wings"
+    
+
+
+},
+{
+    image: "/public/menu/chips.jpg",
+    price: 8,
+    title:"Hot Chips"
+
+
+},
+{
+    image: "/public/menu/Cesar.jpg",
+    price: 11,
+    title:"Ceaser Salad",
+    desc: "lettuce, bacon, cheese, fried egg, caesar sauce and croutons"
+    
+
+
+},
+
+{
+    image: "/public/menu/Patato.jpg",
+    price: 8,
+    title:"Sweet Potato Fries",
+    desc: "Sweet Potato Fries"
+    
+
+
+},
+
+
+]
